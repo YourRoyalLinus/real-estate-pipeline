42405145972d7b4becd4a2b6ca6ae95bc30377ee
YourRoyalLinus <jonbillings1996@gmail.com>
2022-04-06 11:35:30 -0400

    Fully implemented transformation task
    A DatabricksSubmitRunOperator task loads data from the
    S3 unstructured bucket and stages and models the data in
    delta tables on the Databricks cluster. A subsequent task
    to load the data into the data warehouse should be
    implemented. New keys "spark_cluster_id" and
    "spark_notebook_path" added to properties_batch_config
    Variable.
    
    The notebook(s) associated with this task have also been
    added to this repo and can be managed by Databricks by
    configuring Git integration using Personal Access Tokens.
    It should be noted that Community Editions of Databricks
    will not work with this task as as the Community Edition
    doesn’t allow for API access, which is how the operator
    triggers the task.

--- dags/batch_load.py
+++ dags/batch_load.py
@@ -3,15 +3,14 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.task_group import TaskGroup
 from airflow.models.variable import Variable
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
 from include.operators.IngestionOperators import IngressAPIDataOperator
 from include.operators.IngestionOperators import PreProcessDataOperator
 
 default_args = {
     "depends_on_past" : False,
-    "catchup" : False,
     "retries" : 0
 }
-
 #Reading these is delayed until task execution
 timestamp = "{{ ds }}"
 last_success_timestamp = "{{ prev_start_date_success }}"
@@ -20,9 +19,10 @@ endpoints = ["list-sold", "list-for-rent", "list-for-sale"]
 with DAG(dag_id="daily_real_estate_api_batch_load", 
     description="Fetch the daily batch of data from the Real Estate API",
     schedule_interval='@daily',
-    start_date=datetime(2022, 3, 12, 17, 50, 0),
-    end_date=datetime(2022, 4, 1, 11, 59, 59),
+    start_date=datetime(2022, 4, 1, 0, 0, 0),
+    end_date=datetime(2022, 4, 29, 0, 0, 0),
     render_template_as_native_obj=True,
+    catchup=False,
     default_args=default_args
 ) as dag:
     batch_config = Variable.get("properties_batch_config", 
@@ -62,7 +62,22 @@ with DAG(dag_id="daily_real_estate_api_batch_load",
                 s3_bucket_region=batch_config["s3_bucket_region"],
                 s3_bucket_key=s3_bucket_key
             )
+                        
+            transform_data = DatabricksSubmitRunOperator(
+                task_id="transform_{}_data".format(endpoint_fmt), 
+                existing_cluster_id=batch_config["spark_cluster_id"], 
+                notebook_task={
+                    "notebook_path": batch_config["spark_notebook_path"], 
+                    "base_parameters": {
+                        "aws_bucket_name": batch_config["s3_unstructured_bucket"],
+                        "aws_region": batch_config["s3_bucket_region"],
+                        "data_endpoint": endpoint
+                    }
+                }
+            )
 
-            fetch_and_store_data >> preprocess_stored_data
-  
+            fetch_and_store_data >> \
+            preprocess_stored_data >> \
+            transform_data
+
     start >> ingest_api_data >> end
